b9325cc9218ab160b6174e1e58a086db3434103d
steveasterafovo <steveasterafovo@gmail.com>
2026-08-23 16:22:38 +0100

    s

--- src/components/site/ArcadeShell.tsx
+++ src/components/site/ArcadeShell.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react";
 import { PageHeader, PageFooter } from "@/components/site";
+import { CookieConsent } from "@/components/site";
 
 interface ArcadeShellProps {
   children: ReactNode;
@@ -14,6 +15,8 @@ export function ArcadeShell({ children, headerContent }: ArcadeShellProps) {
         {children}
       </main>
       <PageFooter />
+
+      <CookieConsent />
     </div>
   );
 }
