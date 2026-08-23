d49263bf0308ddb574ad4174cc6391fb6a21a3ff
steveasterafovo <steveasterafovo@gmail.com>
2026-08-23 20:31:23 +0100

    s

--- src/components/arcade/share-progress.tsx
+++ src/components/arcade/share-progress.tsx
@@ -5,7 +5,7 @@ import { WhatsAppIcon, FacebookIcon, LinkedinIcon, XIcon } from "@/components/si
 import { Button } from "@/components/ui/button";
 
 function shareUrl() {
-  if (typeof window === "undefined") return SITE?.arcadeUrl ?? "https://arcade.stafprint.com";
+  if (typeof window === "undefined") return SITE.arcadeUrl;
   return `${window.location.origin}`;
 }
 
