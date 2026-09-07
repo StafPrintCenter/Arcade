286e8dda8bca6ffaebc0de04485150ab76db5f92
steveasterafovo <steveasterafovo@gmail.com>
2026-09-07 12:15:07 +0100

    s

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -23,6 +23,16 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       /* Open Graph / Facebook / WhatsApp */
       { property: "og:title", content: ARCADE_TITLE },
       { property: "og:description", content: ARCADE_DESC },
+      { property: "og:type", content: "website" },
+      { property: "og:site_name", content: `SPC Docs - ${SITE.name}` },
+      { property: "og:image", content: `${logo.meta}` },
+      { property: "og:image:width", content: "1200" },
+      { property: "og:image:height", content: "630" },
+      { property: "og:image:alt", content: `Documentation Officielle ${SITE.name}` },
+      { property: "og:url", content: `${SITE_LINK.docsUrl}` },
+      { property: "og:locale", content: "fr_BJ" },
+
+      /* Twitter / X */
       { name: "twitter:card", content: "summary_large_image" },
       { name: "twitter:title", content: ARCADE_TITLE },
       { name: "twitter:description", content: ARCADE_DESC },
