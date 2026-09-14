42319a2c2e51e25198ab63fe0005e464af32cd53
steveasterafovo <steveasterafovo@gmail.com>
2026-09-14 18:06:26 +0100

    s

--- src/components/errors/NotFound.tsx
+++ src/components/errors/NotFound.tsx
@@ -1,6 +1,5 @@
 import { Link } from "@tanstack/react-router";
 import { RotateCcw, ArrowLeft, Gamepad2 } from "lucide-react";
-import { Button } from "@/components/ui/button";
 
 export function NotFoundComponent() {
   return (
