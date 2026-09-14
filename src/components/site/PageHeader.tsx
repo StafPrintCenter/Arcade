f4b9e0941e82f4c526a660f935acdfd222d6d065
steveasterafovo <steveasterafovo@gmail.com>
2026-09-14 13:16:04 +0100

    s

--- src/components/site/PageHeader.tsx
+++ src/components/site/PageHeader.tsx
@@ -1,6 +1,7 @@
 import { Link } from "@tanstack/react-router";
 import logo from "@/assets/logos.json";
 import { ThemeToggle } from "@/components/arcade/themeToggle";
+import { SpcMobLogo, SpcDeskLogo } from "@/components/site/SpcLogo";
 
 interface PageHeaderProps {
   children?: React.ReactNode;
