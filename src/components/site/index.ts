00e2115093a752260f95b9e1e85dbc9e0c5cea33
steveasterafovo <steveasterafovo@gmail.com>
2026-09-14 13:18:12 +0100

    s

--- src/components/site/index.ts
+++ src/components/site/index.ts
@@ -1,2 +1,6 @@
 export { PageHeader } from "./PageHeader";
 export { PageFooter } from "./PageFooter";
+export { ThemeToggle } from "./ThemeToggle";
+export { SpcMobLogo, SpcDeskLogo } from "./SpcLogo";
+export { ArcadeShell } from "./ArcadeShell";
+export { updateGaConsent } from "./CookieConsent";
